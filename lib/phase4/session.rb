bb7478472413830143543310c50add6e3fe3b56c
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-11-04 14:46:40 -0800

    move project out of skeleton folder, complete session class

---
+++ lib/phase4/session.rb
@@ -0,0 +1,33 @@
+require 'json'
+require 'webrick'
+
+module Phase4
+  class Session
+    # find the cookie for this app
+    # deserialize the cookie into a hash
+    def initialize(req)
+      req.cookies.each do |potential_cookie|
+        if potential_cookie.name == "ruby-on-tracks"
+          @cookie_value = JSON.parse(potential_cookie.value)
+          return @cookie_value
+        end
+      end
+      @cookie_value = Hash.new
+    end
+
+    def [](key)
+      @cookie_value[key]
+    end
+
+    def []=(key, value)
+      @cookie_value[key] = value
+    end
+
+    # serialize the hash into json and save in a cookie
+    # add to the responses cookies
+    def store_session(res)
+      cookie = WEBrick::Cookie.new("ruby-on-tracks", @cookie_value.to_json)
+      res.cookies << cookie
+    end
+  end
+end
