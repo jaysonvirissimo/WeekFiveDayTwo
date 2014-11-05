23cfed2d4d926df14a3eef1b54a6d7f88e1d63f5
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-11-04 17:28:53 -0800

    remove extraneous underscores

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -2,16 +2,16 @@ module Phase6
   class Route
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
-    def initialize(_pattern, _http_method, _controller_class, _action_name)
+    def initialize(pattern, http_method, controller_class, action_name)
     end
 
     # checks if pattern matches path and method matches request method
-    def matches?(_req)
+    def matches?(req)
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
-    def run(_req, _res)
+    def run(req, res)
     end
   end
 
@@ -22,25 +22,25 @@ module Phase6
     end
 
     # simply adds a new route to the list of routes
-    def add_route(_pattern, _method, _controller_class, _action_name)
+    def add_route(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
     # for syntactic sugar :)
-    def draw(&_proc)
+    def draw(&proc)
     end
 
     # make each of these methods that
     # when called add route
-    [:get, :post, :put, :delete].each do |_http_method|
+    [:get, :post, :put, :delete].each do |http_method|
     end
 
     # should return the route that matches this request
-    def match(_req)
+    def match(req)
     end
 
     # either throw 404 or call run on a matched route
-    def run(_req, _res)
+    def run(req, res)
     end
   end
 end
