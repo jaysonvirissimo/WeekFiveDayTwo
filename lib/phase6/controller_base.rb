0de11aea490a0b85290bb07feb99f6b31384bd04
Jayson Virissimo <jayson.virissimo@asu.edu>
2014-11-05 10:29:20 -0800

    started phase 6 controller base and router, incorrent output for matches method

--- lib/phase6/controller_base.rb
+++ lib/phase6/controller_base.rb
@@ -3,7 +3,10 @@ require_relative '../phase5/controller_base'
 module Phase6
   class ControllerBase < Phase5::ControllerBase
     # use this with the router to call action_name (:index, :show, :create...)
-    def invoke_action(_name)
+    def invoke_action(method_name)
+      send(method_name)
+      render(method_name) unless already_built_response?
+      nil
     end
   end
 end
